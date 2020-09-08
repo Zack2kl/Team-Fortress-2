287aa662cad6930017cf7a112d7f712f808c30e1
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-09-08 13:06:40 -0400

    Update Spectators.lua

--- Spectators.lua
+++ Spectators.lua
@@ -3,7 +3,7 @@ local shorten = function(s, m)local t,w,c='',0 for i=1,#s do t=t..s:sub(i,i)w=dr
 local window = gui.Window( 'spectate_window', 'Spectator List', 200, 200, 175, 32 )
 local enabled = gui.Checkbox( gui.Reference('Misc', 'Part 3'), 'spectator_list', 'Show Spectators', false )
 local hide_list = gui.Checkbox( gui.Reference('Misc', 'Part 3'), 'spectator_list_hide', 'Hide list if not spectated', false )
-local fov_change = gui.Slider( gui.Reference('Misc', 'Part 3'), 'spectator_list_fov', 'Reduce FOV', -1, 0, 180 )
+local fov_change = gui.Slider( gui.Reference('Misc', 'Part 3'), 'spectator_list_fov', 'Reduce FOV', -1, -1, 180 )
 local players = {}
 local cached, set
 
